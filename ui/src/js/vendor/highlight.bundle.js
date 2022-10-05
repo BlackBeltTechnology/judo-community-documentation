5beb7172f381229bb9faea7b5dae3969bb564749
Norbert Herczeg <noherczeg@gmail.com>
2022-10-05 19:38:38 +0200

    JNG-4100: bump highlight.js version

--- ui/src/js/vendor/highlight.bundle.js
+++ ui/src/js/vendor/highlight.bundle.js
@@ -2,19 +2,64 @@
   'use strict'
 
   function jsl (hljs) {
-    var LITERALS = 'true false implies or xor and div mod not'
+    var LITERALS = [
+      'true',
+      'false',
+      'implies',
+      'or',
+      'xor',
+      'and',
+      'div',
+      'mod',
+      'not',
+    ]
 
-    var BUILT_INS = 'opposite opposite-add read-only max-length mime-types max-file-size ' +
-      'regex precision required scale min-size max-size'
+    var BUILT_INS = [
+      'opposite',
+      'opposite-add',
+      'read-only',
+      'max-length',
+      'mime-types',
+      'max-file-size',
+      'regex',
+      'precision',
+      'required',
+      'scale',
+      'min-size',
+      'max-size',
+    ]
 
-    var TYPES = 'binary string numeric timestamp date time boolean'
+    var TYPES = [
+      'binary',
+      'string',
+      'numeric',
+      'timestamp',
+      'date',
+      'time',
+      'boolean',
+    ]
 
-    var KWS =
-      'constraint field identifier derived relation static new ' +
-      'self abstract extends model entity query type error enum'
+    var KWS = [
+      'constraint',
+      'field',
+      'identifier',
+      'derived',
+      'relation',
+      'static',
+      'new ',
+      'self',
+      'abstract',
+      'extends',
+      'model',
+      'entity',
+      'query',
+      'type',
+      'error',
+      'enum',
+    ]
 
     var KEYWORDS = {
-      //$pattern: /[a-zA-Z-]+/,
+      $pattern: /[a-zA-Z-]+/,
       keyword: KWS,
       type: TYPES,
       literal: LITERALS,
@@ -38,24 +83,9 @@
     }
   }
 
-  var hljs = require('highlight.js/lib/highlight')
-  hljs.registerLanguage('asciidoc', require('highlight.js/lib/languages/asciidoc'))
-  hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
-  hljs.registerLanguage('diff', require('highlight.js/lib/languages/diff'))
-  hljs.registerLanguage('dockerfile', require('highlight.js/lib/languages/dockerfile'))
-  hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
-  hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
-  hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
-  hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
-  hljs.registerLanguage('properties', require('highlight.js/lib/languages/properties'))
-  hljs.registerLanguage('puppet', require('highlight.js/lib/languages/puppet'))
-  hljs.registerLanguage('python', require('highlight.js/lib/languages/python'))
-  hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'))
-  hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
-  hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
-  hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'))
+  var hljs = require('highlight.js/lib/common')
   hljs.registerLanguage('jsl', jsl)
   ;[].slice.call(document.querySelectorAll('pre code.hljs')).forEach(function (node) {
-    hljs.highlightBlock(node)
+    hljs.highlightElement(node)
   })
 })()
