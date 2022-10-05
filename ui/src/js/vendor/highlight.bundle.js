8ed51b4827a164e4db6708590714286d03e831b3
Norbert Herczeg <noherczeg@gmail.com>
2022-10-05 18:51:49 +0200

    JNG-4100: jsl syntax highlight

--- ui/src/js/vendor/highlight.bundle.js
+++ ui/src/js/vendor/highlight.bundle.js
@@ -1,39 +1,56 @@
 ;(function () {
   'use strict'
 
+  function jsl (hljs) {
+    var LITERALS = 'true false implies or xor and div mod not'
+
+    var BUILT_INS = 'opposite opposite-add read-only max-length mime-types max-file-size ' +
+      'regex precision required scale min-size max-size'
+
+    var TYPES = 'binary string numeric timestamp date time boolean'
+
+    var KWS =
+      'constraint field identifier derived relation static new ' +
+      'self abstract extends model entity query type error enum'
+
+    var KEYWORDS = {
+      //$pattern: /[a-zA-Z-]+/,
+      keyword: KWS,
+      type: TYPES,
+      literal: LITERALS,
+      built_in: BUILT_INS,
+    }
+
+    return {
+      aliases: ['jsl', 'JSL'],
+      keywords: KEYWORDS,
+      contains: [
+        hljs.QUOTE_STRING_MODE,
+        hljs.NUMBER_MODE,
+        hljs.BACKSLASH_ESCAPE,
+        hljs.C_LINE_COMMENT_MODE,
+        hljs.C_BLOCK_COMMENT_MODE,
+      ],
+    }
+  }
+
   var hljs = require('highlight.js/lib/highlight')
   hljs.registerLanguage('asciidoc', require('highlight.js/lib/languages/asciidoc'))
   hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
-  hljs.registerLanguage('clojure', require('highlight.js/lib/languages/clojure'))
-  hljs.registerLanguage('cpp', require('highlight.js/lib/languages/cpp'))
-  hljs.registerLanguage('cs', require('highlight.js/lib/languages/cs'))
-  hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
   hljs.registerLanguage('diff', require('highlight.js/lib/languages/diff'))
   hljs.registerLanguage('dockerfile', require('highlight.js/lib/languages/dockerfile'))
-  hljs.registerLanguage('elixir', require('highlight.js/lib/languages/elixir'))
-  hljs.registerLanguage('go', require('highlight.js/lib/languages/go'))
-  hljs.registerLanguage('groovy', require('highlight.js/lib/languages/groovy'))
-  hljs.registerLanguage('haskell', require('highlight.js/lib/languages/haskell'))
   hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
   hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
   hljs.registerLanguage('json', require('highlight.js/lib/languages/json'))
-  hljs.registerLanguage('kotlin', require('highlight.js/lib/languages/kotlin'))
   hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
-  hljs.registerLanguage('nix', require('highlight.js/lib/languages/nix'))
-  hljs.registerLanguage('objectivec', require('highlight.js/lib/languages/objectivec'))
-  hljs.registerLanguage('perl', require('highlight.js/lib/languages/perl'))
-  hljs.registerLanguage('php', require('highlight.js/lib/languages/php'))
   hljs.registerLanguage('properties', require('highlight.js/lib/languages/properties'))
   hljs.registerLanguage('puppet', require('highlight.js/lib/languages/puppet'))
   hljs.registerLanguage('python', require('highlight.js/lib/languages/python'))
-  hljs.registerLanguage('ruby', require('highlight.js/lib/languages/ruby'))
-  hljs.registerLanguage('rust', require('highlight.js/lib/languages/rust'))
-  hljs.registerLanguage('scala', require('highlight.js/lib/languages/scala'))
   hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'))
   hljs.registerLanguage('sql', require('highlight.js/lib/languages/sql'))
-  hljs.registerLanguage('swift', require('highlight.js/lib/languages/swift'))
   hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
   hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'))
+  hljs.registerLanguage('jsl', jsl)
   ;[].slice.call(document.querySelectorAll('pre code.hljs')).forEach(function (node) {
     hljs.highlightBlock(node)
   })
